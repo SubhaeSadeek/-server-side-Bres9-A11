8c0c8f6f769935f17dfd2e0b0c3918b010dbe41d
SubhaeSadeek <nurun.nabi@outlook.com>
2025-05-02 09:36:37 +0600

    Alhamdulillah, server side initiated

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+// adding middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+// getting the server live 
+app.get("/", (req, res)=>{
+    res.send("Alhamdulillah, server is getting ready for assignment 11");
+
+});
+app.listen(port, ()=>{
+    console.log(`app is listening to port${port}`);
+})
